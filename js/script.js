96d4bb0e7aaf56fcbeca2fac77fca39aa2122140
WalterCorsini <corsini.walter@gmail.com>
2024-04-29 14:53:57 +0200

    fix with methods

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            arrayMail: [],
-        }
+  data() {
+    return {
+      arrayMail: [],
+    };
+  },
+  created() {
+    this.createdEmails();
+  },
+  methods: {
+      createdEmails: function () {
+          for (i = 0; i < 10; i++) {
+              axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+              .then((rndMail) => {
+                  this.arrayMail.push(rndMail.data.response);
+                  console.log("email random", this.arrayMail);
+                });
+            }
+        },
     },
-    created(){
-        for(i=0; i<10; i++){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((rndMail) =>{
-            this.arrayMail.push(rndMail.data.response);
-            console.log("email random", this.arrayMail);
-        })
-    }
-    }
 }).mount("#app");
